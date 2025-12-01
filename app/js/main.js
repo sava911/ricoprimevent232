046841b2621e3d56aaae6573b721b4f9738cd2cc
sava911 <pesss929@gmail.com>
2025-12-01 13:58:36 +0300

    end stages section

--- app/js/main.js
+++ app/js/main.js
@@ -83,4 +83,89 @@ $(function () {
     $('.header__nav').removeClass('active');
   });
 
+
+
+  // count
+  let counted = false;
+
+  $.fn.jQuerySimpleCounter = function (options) {
+    var settings = $.extend({
+      start: 0,
+      end: 100,
+      easing: 'swing',
+      duration: 400,
+      complete: ''
+    }, options);
+
+    var thisElement = $(this);
+
+    $({count: settings.start}).animate({count: settings.end}, {
+      duration: settings.duration,
+      easing: settings.easing,
+      step: function () {
+        var mathCount = Math.ceil(this.count);
+        thisElement.text(mathCount);
+      },
+      complete: settings.complete
+    });
+  };
+
+  function startCounter() {
+    if (!counted) {
+      $('#number1').jQuerySimpleCounter({end: 12, duration: 3000});
+      $('#number2').jQuerySimpleCounter({end: 55, duration: 3000});
+      $('#number3').jQuerySimpleCounter({end: 359, duration: 2000});
+      $('#number4').jQuerySimpleCounter({end: 246, duration: 2500});
+      counted = true;
+    }
+  }
+
+  // Запуск счетчика при прокрутке до блока
+  $(window).on('scroll', function () {
+    var counterPos = $('.counter').offset().top;
+    var scrollPos = $(window).scrollTop() + $(window).height();
+
+    if (scrollPos > counterPos && !counted) {
+      startCounter();
+    }
+  });
+
+  $.fn.jQuerySimpleCounter = function (options) {
+    var settings = $.extend({
+      start: 0,
+      end: 100,
+      easing: 'swing',
+      duration: 400,
+      complete: ''
+    }, options);
+
+    var thisElement = $(this);
+
+    $({count: settings.start}).animate({count: settings.end}, {
+      duration: settings.duration,
+      easing: settings.easing,
+      step: function () {
+        var mathCount = Math.ceil(this.count);
+        thisElement.text(mathCount);
+      },
+      complete: settings.complete
+    });
+  };
+
+
+  $('#number1').jQuerySimpleCounter({end: 12, duration: 3000});
+  $('#number2').jQuerySimpleCounter({end: 55, duration: 3000});
+  $('#number3').jQuerySimpleCounter({end: 359, duration: 2000});
+  $('#number4').jQuerySimpleCounter({end: 246, duration: 2500});
+
+
+
+  /* AUTHOR LINK */
+  $('.about-me-img').hover(function () {
+    $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+  }, function () {
+    $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+  });
+
+
 })
